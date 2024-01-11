6c72ba2a838546e87a4f9dcb67b3c9541d077d1c
Trip cubbison <119246941+TripCu@users.noreply.github.com>
2024-01-10 22:15:07 -0500

    weds

--- src/Watcher.java
+++ src/Watcher.java
@@ -7,11 +7,10 @@ public class Watcher {
 
                 int exitCode = process.waitFor();
                 if (exitCode == 0) {
-                    // Exit code 0 indicates intentional shutdown via F12 and passcode
                     break; // Exit the watcher loop
                 } else {
-                    // The application was closed unexpectedly, restart it
                     System.out.println("DuckSprite was closed unexpectedly. Restarting...");
+
                 }
             } catch (Exception e) {
                 e.printStackTrace();
